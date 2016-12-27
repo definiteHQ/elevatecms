91d878a54299bcf8fcae034de25ff8fc4fe92f5d
Dharma Saputra <mail.dharma.saputra@gmail.com>
2016-12-27 11:06:24 +0700

    [UPDATE] Redirect after login url

--- tests/acceptance/LoginCest.php
+++ tests/acceptance/LoginCest.php
@@ -17,6 +17,6 @@ class LoginCest
         $I->fillField('email', 'admin@demo.com');
         $I->fillField('password', 'demo12345');
         $I->click('Login');
-        $I->seeCurrentUrlEquals('dashboard');
+        $I->seeCurrentUrlEquals('backend');
     }
 }
