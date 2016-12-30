8d54041e31cfd7f3c4172d86211a843554ae7ca7
Dharma Saputra <mail.dharma.saputra@gmail.com>
2016-12-30 10:36:36 +0700

    [ADD] Logout link

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -143,7 +143,7 @@
                 </li>
 
                 <li>
-                    <a class="logout" href="javascript:;">
+                    <a class="logout" href="/auth/logout">
                         <i class="ti-power-off"></i>
                         <p>Logout</p>
                     </a>
@@ -173,7 +173,7 @@
                               </a>
                               <ul class="dropdown-menu">
                                 <li>
-                                    <a class="logout" href="javascript:;">Logout</a>
+                                    <a class="logout" href="/auth/logout">Logout</a>
                                 </li>
                               </ul>
                         </li>
