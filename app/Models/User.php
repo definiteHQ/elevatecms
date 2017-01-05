3d6c1a1849b9066bd90505cff0f9ba686c1b3ea9
Dharma Saputra <mail.dharma.saputra@gmail.com>
2017-01-05 13:42:04 +0700

    [ADD] Zizaco Package for User Role Management

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Database\Eloquent\Model;
+use Zizaco\Entrust\Traits\EntrustUserTrait;
 
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract
 {
-    use Authenticatable, CanResetPassword;
+    use Authenticatable, CanResetPassword, EntrustUserTrait;
 
     /**
      * The database table used by the model.
