7656137a560c84e1b8614b6a3e3d68833bd2f1a3
alfianasdianto <alfian04@live.com>
2017-01-17 11:44:19 +0700

    [UPDATE] function for generate analytics

--- app/helpers.php
+++ app/helpers.php
@@ -1,4 +1,5 @@
 <?php
+use App\Models\Setting;
 
 if (!function_exists('error_widget'))
 {
@@ -46,5 +47,20 @@ if (!function_exists('deleteFile'))
         }
 
         return false;
+    }
+}
+
+if (!function_exists('google_analytics'))
+{
+    function google_analytics()
+    {
+        $html = "";
+        $settings = Setting::where('key', 'site_ga')->first();
+
+        if ( $settings->count() > 0 AND !empty($settings->value) ) {
+            $html = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ <br> (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),<br>m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)<br>})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');<br><br>ga('create', '".$settings->value."', 'auto');<br>ga('send', 'pageview');";
+        }
+
+        return $html;
     }
 }
